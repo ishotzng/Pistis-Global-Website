8e9b8261a317a1d626724600bb86887cfdf6786f
kelv <kelvhanson@gmail.com>
2026-01-28 13:21:09 +0100

    Fix missing 'docker' command in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-sudo stop taxi-frontend
+sudo docker stop taxi-frontend
 sudo docker rm taxi-frontend
 sudo docker build -t taxi-frontend .
 sudo docker run -d -p 80:80 --name taxi-frontend taxi-frontend
